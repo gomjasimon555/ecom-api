3b922b9eb5445032653022254553c24ce9972910
Simon Tamang <gomjasimon555@gmail.com>
2023-05-25 19:26:10 +0545

    database connection

--- index.js
+++ index.js
@@ -4,14 +4,21 @@ const cors=require('cors')
 const app = express();
 const homeRouter = require("./routes/home");
 const productRouter = require("./routes/product");
+const connectDatabase = require('./database/connection');
    
 const logger = require('./middlewares/logger');
 const morgan=require('morgan')
+
+//Setting up ENV in our project
+require('dotenv').config();
 
 //Global middleware
 app.use(cors()); 
 app.use(logger)
 app.use(morgan('dev'))
+
+//DB Connection
+connectDatabase();
 
 app.use(homeRouter);
 app.use("/api/products",productRouter);
@@ -19,9 +26,9 @@ app.use("/api/products",productRouter);
 app.get("*",(req,res)=>{
     res.send("<h1> Provided Index not found </h1>")
 })
-port =8080
-app.listen(port,(req,res)=>{
-    console.log(`server is running on port ${port}`)
+
+app.listen(process.env.PORT,(req,res)=>{
+    console.log(`server is running on ${process.env.PORT}`)
 });
 
 
